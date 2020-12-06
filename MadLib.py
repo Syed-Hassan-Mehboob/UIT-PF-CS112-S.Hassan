fde5142a25d3f3a3c24dba07c32bccd84d569054
Syed Hassan Mehboob <mehboobhassan20032@gmail.com>
2020-12-07 04:30:01 +0500

    Add files via upload
    My quote unquote first Python 3 program.

---
+++ MadLib.py
@@ -0,0 +1,6 @@
+adjective = input("please enter an adjective: ")
+noun = input("please enter a noun: ")
+verb = input("please enter a verb ending in -ed: ")
+animal = input("please enter a name of an animal: ")
+print("your Madlib:")
+print("the", adjective, noun, verb, "over the lazy brown", animal)
